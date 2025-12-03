7c286a9889c8068191b2a0213d066192d79aa5b0
Maria Paula Herrera <herrerapaula2005@gmail.com>
2025-12-03 13:13:55 -0500

    feat: componente Todos.jsx con fetch inicial usando useEffect

--- todo-app/src/pages/Todos.jsx
+++ todo-app/src/pages/Todos.jsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function Todos() {
-  return <h1>Lista de Todos</h1>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de Todos</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
